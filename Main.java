fb5a208ee6f33e12e565059b5f321c2ce4056338
Екатерина Сочкова <volontare@yandex.ru>
2021-03-14 15:37:09 +0400

    Added Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        long amount = 56_314_20;
+        int part = 20;
+
+        long milеs = amount/part / 100;
+        System.out.println(milеs);
+    }
+}
